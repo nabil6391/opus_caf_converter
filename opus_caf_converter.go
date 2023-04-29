037dab7d74513a1d5f952f1f96d67cc0bac9ebb3
Nabil <nabil6391@gmail.com>
2023-04-29 17:58:02 +0600

    Update Code

--- opus_caf_converter.go
+++ opus_caf_converter.go
@@ -4,10 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/binary"
-	"encoding/hex"
 	"errors"
-	"fmt"
-	"hash/crc32"
 	"io"
 	"math"
 	"os"
@@ -15,6 +12,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// CAF Encoding and Decoding
 type FourByteString [4]byte
 
 var ChunkTypeAudioDescription = stringToChunkType("desc")
@@ -154,11 +152,6 @@ type ChannelDescription struct {
 	Coordinates  [3]float32
 }
 
-type Information struct {
-	Key   string
-	Value string
-}
-
 type UnknownContents struct {
 	Data []byte
 }
@@ -199,6 +192,11 @@ func (cf *File) Encode(w io.Writer) error {
 		}
 	}
 	return nil
+}
+
+type Information struct {
+	Key   string
+	Value string
 }
 
 func readString(r io.Reader) (string, error) {
@@ -245,6 +243,11 @@ func (c *Information) encode(w io.Writer) error {
 	return writeString(w, c.Value)
 }
 
+type CAFStringsChunk struct {
+	NumEntries uint32
+	Strings    []Information
+}
+
 func (c *CAFStringsChunk) decode(r io.Reader) error {
 	if err := binary.Read(r, binary.BigEndian, &c.NumEntries); err != nil {
 		return err
@@ -269,11 +272,6 @@ func (c *CAFStringsChunk) encode(w io.Writer) error {
 		}
 	}
 	return nil
-}
-
-type CAFStringsChunk struct {
-	NumEntries uint32
-	Strings    []Information
 }
 
 type Chunk struct {
@@ -513,262 +511,7 @@ func (h *FileHeader) Encode(w io.Writer) error {
 	return nil
 }
 
-const DEFAULT_BUFFER_FOR_PLAYBACK_MS = 2500
-
-// OpusReader is used to take an OGG file and write RTP packets
-type OpusReader struct {
-	stream                  io.Reader
-	fd                      *os.File
-	SampleRate              uint32
-	Channels                uint8
-	serial                  uint32
-	pageIndex               uint32
-	checksumTable           *crc32.Table
-	previousGranulePosition uint64
-	currentSampleLen        float32
-	CurrentSampleLen        uint32
-	CurrentFrames           uint32
-	currentSamples          uint32
-	currentSegment          uint8
-	payloadLen              uint32
-	segments                uint8
-	currentSample           uint8
-	segmentMap              map[uint8]uint8
-}
-
-type OpusSamples struct {
-	Payload  []byte
-	Frames   uint8
-	Samples  uint32
-	Duration uint32
-}
-
-// New builds a new OGG Opus reader
-func NewFile(fileName string) (*OpusReader, error) {
-	f, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
-	}
-	reader := &OpusReader{}
-	//  reader, err := NewWith(f, sampleRate, channelCount)
-	//if err != nil {
-	//        return nil, err
-	//}
-	reader.fd = f
-	reader.segmentMap = make(map[uint8]uint8)
-	reader.stream = bufio.NewReader(f)
-	err = reader.getPage()
-	if err != nil {
-		return reader, err
-	}
-	err = reader.getPage()
-	if err != nil {
-		return reader, err
-	}
-	return reader, nil
-}
-
-func (i *OpusReader) readOpusHead() error {
-	var version uint8
-	magic := make([]byte, 8)
-	if err := binary.Read(i.stream, binary.LittleEndian, &magic); err != err {
-		return err
-	}
-	if bytes.Compare(magic, []byte("OpusHead")) != 0 {
-		return errors.New("Wrong Opus Head")
-	}
-
-	if err := binary.Read(i.stream, binary.LittleEndian, &version); err != err {
-		return err
-	}
-	if err := binary.Read(i.stream, binary.LittleEndian, &i.Channels); err != err {
-		return err
-	}
-	var preSkip uint16
-	if err := binary.Read(i.stream, binary.LittleEndian, &preSkip); err != err {
-		return err
-	}
-	if err := binary.Read(i.stream, binary.LittleEndian, &i.SampleRate); err != err {
-		return err
-	}
-	//Skipping OutputGain
-	io.CopyN(io.Discard, i.stream, 2)
-	var channelMap uint8
-	if err := binary.Read(i.stream, binary.LittleEndian, &channelMap); err != err {
-		return err
-	}
-	//if channelMap (Mapping family) is different than 0, next 4 bytes contain channel mapping configuration
-	if channelMap != 0 {
-		io.CopyN(io.Discard, i.stream, 4)
-	}
-	return nil
-}
-
-func (i *OpusReader) readOpusTags() (uint32, error) {
-	var plen uint32
-	var vendorLen uint32
-	magic := make([]byte, 8)
-	if err := binary.Read(i.stream, binary.LittleEndian, &magic); err != err {
-		return 0, err
-	}
-	if bytes.Compare(magic, []byte("OpusTags")) != 0 {
-		return 0, errors.New("Wrong Opus Tags")
-	}
-
-	if err := binary.Read(i.stream, binary.LittleEndian, &vendorLen); err != err {
-		return 0, err
-	}
-	vendorName := make([]byte, vendorLen)
-	if err := binary.Read(i.stream, binary.LittleEndian, &vendorName); err != err {
-		return 0, err
-	}
-
-	var userCommentLen uint32
-	if err := binary.Read(i.stream, binary.LittleEndian, &userCommentLen); err != err {
-		return 0, err
-	}
-	userComment := make([]byte, userCommentLen)
-	if err := binary.Read(i.stream, binary.LittleEndian, &userComment); err != err {
-		return 0, err
-	}
-	plen = 16 + vendorLen + userCommentLen
-	return plen, nil
-
-}
-
-func (i *OpusReader) getPageHead() error {
-	head := make([]byte, 4)
-	if err := binary.Read(i.stream, binary.LittleEndian, &head); err != err {
-		return err
-	}
-	if bytes.Compare(head, []byte("OggS")) != 0 {
-		return fmt.Errorf("Incorrect page. Does not start with \"OggS\" : %s %v", string(head), hex.EncodeToString(head))
-	}
-	//Skipping Version
-	io.CopyN(io.Discard, i.stream, 1)
-	var headerType uint8
-	if err := binary.Read(i.stream, binary.LittleEndian, &headerType); err != err {
-		return err
-	}
-	var granulePosition uint64
-	if err := binary.Read(i.stream, binary.LittleEndian, &granulePosition); err != err {
-		return err
-	}
-	if err := binary.Read(i.stream, binary.LittleEndian, &i.serial); err != err {
-		return err
-	}
-	if err := binary.Read(i.stream, binary.LittleEndian, &i.pageIndex); err != err {
-		return err
-	}
-	//skipping checksum
-	io.CopyN(io.Discard, i.stream, 4)
-
-	if err := binary.Read(i.stream, binary.LittleEndian, &i.segments); err != err {
-		return err
-	}
-	var x uint8
-	// building a map of all segments
-	i.payloadLen = 0
-	for x = 1; x <= i.segments; x++ {
-		var segSize uint8
-		if err := binary.Read(i.stream, binary.LittleEndian, &segSize); err != err {
-			return err
-		}
-		i.segmentMap[x] = segSize
-		i.payloadLen += uint32(segSize)
-	}
-
-	return nil
-}
-
-func (i *OpusReader) getPage() error {
-	err := i.getPageHead()
-	if err != nil {
-		return err
-	}
-	if i.pageIndex == 0 {
-		err := i.readOpusHead()
-		if err != nil {
-			return err
-		}
-	} else if i.pageIndex == 1 {
-		plen, err := i.readOpusTags()
-		if err != nil {
-			return err
-		}
-		// we are not interested in tags (metadata?)
-		io.CopyN(io.Discard, i.stream, int64(i.payloadLen-plen))
-
-	} else {
-		io.CopyN(io.Discard, i.stream, int64(i.payloadLen))
-	}
-
-	return nil
-}
-
-func (i *OpusReader) GetSample() (*OpusSamples, error) {
-	opusSamples := new(OpusSamples)
-	if i.currentSegment == 0 {
-
-		err := i.getPageHead()
-		if err != nil {
-			return opusSamples, err
-
-		}
-	}
-	var currentPacketSize uint32
-	// Iteraring throug all segments to check if there are lacing packets. If a segment is 255 bytes long, it means that there must be a following segment for the same packet (which may be again 255 bytes long)
-	for i.segmentMap[i.currentSegment] == 255 {
-		currentPacketSize += 255
-		i.currentSegment += 1
-
-	}
-	// Adding either the last segments of lacing ones or a packet that fits only in one segment
-	currentPacketSize += uint32(i.segmentMap[i.currentSegment])
-	if i.currentSegment < i.segments {
-		i.currentSegment += 1
-	} else {
-		i.currentSegment = 0
-	}
-	tmpPacket := make([]byte, currentPacketSize)
-	opusSamples.Payload = tmpPacket
-	binary.Read(i.stream, binary.LittleEndian, &tmpPacket)
-	//Reading the TOC byte - we need to know  the frame duration.
-	if len(tmpPacket) > 0 {
-		tmptoc := tmpPacket[0] & 255
-		var frames uint8
-		switch tmptoc & 3 {
-		case 0:
-			frames = 1
-			break
-		case 1:
-		case 2:
-			frames = 2
-			break
-		default:
-			frames = tmpPacket[1] & 63
-			break
-		}
-		opusSamples.Frames = frames
-		tocConfig := tmptoc >> 3
-
-		var length uint32
-		length = uint32(tocConfig & 3)
-		if tocConfig >= 16 {
-			length = DEFAULT_BUFFER_FOR_PLAYBACK_MS << length
-		} else if tocConfig >= 12 {
-			length = 10000 << (length & 1)
-		} else if length == 3 {
-			length = 60000
-		} else {
-			length = 10000 << length
-		}
-		opusSamples.Duration = length
-		opusSamples.Samples = (i.SampleRate * length) / 1000000
-	}
-	return opusSamples, nil
-}
-
+// Opus Decoding
 const (
 	pageHeaderTypeBeginningOfStream = 0x02
 	pageHeaderSignature             = "OggS"
@@ -786,15 +529,11 @@ var (
 	errBadIDPageLength           = errors.New("payload for id page must be 19 bytes")
 	errBadIDPagePayloadSignature = errors.New("bad payload signature")
 	errShortPageHeader           = errors.New("not enough data for payload header")
-	errChecksumMismatch          = errors.New("expected and actual checksum do not match")
 )
 
 // OggReader is used to read Ogg files and return page payloads
 type OggReader struct {
-	stream               io.Reader
-	bytesReadSuccesfully int64
-	checksumTable        *[256]uint32
-	doChecksum           bool
+	stream io.Reader
 }
 
 // OggHeader is the metadata from the first two pages
@@ -825,21 +564,15 @@ type OggPageHeader struct {
 	segmentsCount uint8
 }
 
-// NewWith returns a new Ogg reader and Ogg header
-// with an io.Reader input
+// NewWith returns a new Ogg reader and Ogg header with an io.Reader input
 func NewWith(in io.Reader) (*OggReader, *OggHeader, error) {
-	return newWith(in /* doChecksum */, true)
-}
 
-func newWith(in io.Reader, doChecksum bool) (*OggReader, *OggHeader, error) {
 	if in == nil {
 		return nil, nil, errNilStream
 	}
 
 	reader := &OggReader{
-		stream:        in,
-		checksumTable: generateChecksumTable(),
-		doChecksum:    doChecksum,
+		stream: in,
 	}
 
 	header, err := reader.readHeaders()
@@ -945,31 +678,6 @@ func (o *OggReader) ParseNextPage() ([][]byte, *OggPageHeader, error) {
 	return segments, pageHeader, nil
 }
 
-// ResetReader resets the internal stream of OggReader. This is useful
-// for live streams, where the end of the file might be read without the
-// data being finished.
-func (o *OggReader) ResetReader(reset func(bytesRead int64) io.Reader) {
-	o.stream = reset(o.bytesReadSuccesfully)
-}
-
-func generateChecksumTable() *[256]uint32 {
-	var table [256]uint32
-	const poly = 0x04c11db7
-
-	for i := range table {
-		r := uint32(i) << 24
-		for j := 0; j < 8; j++ {
-			if (r & 0x80000000) != 0 {
-				r = (r << 1) ^ poly
-			} else {
-				r <<= 1
-			}
-			table[i] = (r & 0xffffffff)
-		}
-	}
-	return &table
-}
-
 func ConvertOpusToCaf(i string, o string) {
 	file, err := os.Open(i)
 	if err != nil {
@@ -1002,7 +710,6 @@ func ConvertOpusToCaf(i string, o string) {
 		for i := range segments {
 			trailing_data = append(trailing_data, uint64(len(segments[i])))
 
-			// packets issues
 			audioData = append(audioData, segments[i]...)
 		}
 
--- opus_caf_converter_test.go
+++ opus_caf_converter_test.go
@@ -2,7 +2,6 @@ package caf
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 	"os/exec"
 	"testing"
@@ -41,38 +40,6 @@ func TestBasicCafEncodingDecoding(t *testing.T) {
 
 func TestConversion(t *testing.T) {
 	ConvertOpusToCaf("samples/sample4.opus", "samples/sample4.caf")
-}
-
-func TestCompareCaf(t *testing.T) {
-	contents1, _ := os.ReadFile("samples/output.caf")
-	contents2, _ := os.ReadFile("file.caf")
-	if len(contents2) != len(contents1) {
-		t.Errorf("contents of input differ when decoding and reencoding, before: %d after: %d",
-			len(contents1),
-			len(contents2))
-	}
-	for i := 0; i < len(contents1); i++ {
-		if contents2[i] != contents1[i] {
-			// print byte content1[i] in fmt.printf as well
-
-			t.Errorf("contents of input differ when decoding and reencoding starting at offset %d %#x %#x", i, contents1[i], contents2[i])
-			break
-		}
-	}
-}
-
-func TestOpusDecoding(t *testing.T) {
-	input := "samples/sample1.opus"
-	opusfile, err := NewFile(input)
-	if err != nil {
-		fmt.Printf("Could not open file %v\n", err.Error())
-	}
-	for i := 0; i <= 5; i++ {
-		_, err = opusfile.GetSample()
-		if err != nil {
-			fmt.Printf("GetSingleSample returned Errr %v\n", err.Error())
-		}
-	}
 }
 
 func TestCompareCafFFMpeg(t *testing.T) {
@@ -114,27 +81,20 @@ func TestCompareCafFFMpeg(t *testing.T) {
 
 }
 
-// func TestMaina(t *testing.T) {
-// 	arr := []int{255, 152, 236, 233, 241, 236, 250, 255, 13, 255, 11, 255, 3, 255, 8, 255, 3, 248, 255, 12, 255, 5, 255, 8, 255, 7, 255, 4, 255, 9, 255, 7, 253, 255, 148, 255, 142, 255, 35, 255, 37, 255, 50, 255, 44, 255, 39, 255, 48, 255, 44, 255, 44, 255, 41, 255, 42, 255, 41, 255, 104, 255, 45, 255}
-// 	newArr := make([]int, 0)
+func TestCompareCaf(t *testing.T) {
+	contents1, _ := os.ReadFile("samples/output.caf")
+	contents2, _ := os.ReadFile("file.caf")
+	if len(contents2) != len(contents1) {
+		t.Errorf("contents of input differ when decoding and reencoding, before: %d after: %d",
+			len(contents1),
+			len(contents2))
+	}
+	for i := 0; i < len(contents1); i++ {
+		if contents2[i] != contents1[i] {
+			// print byte content1[i] in fmt.printf as well
 
-// 	for i := 0; i < len(arr); i++ {
-// 		if i < len(arr) && arr[i] == 255 {
-// 			sum := arr[i]
-// 			i++
-// 			for i < len(arr) && arr[i] == 255 {
-// 				sum += arr[i]
-// 				i++
-// 			}
-// 			if i < len(arr) {
-// 				sum += arr[i]
-// 			}
-
-// 			newArr = append(newArr, sum)
-// 		} else {
-// 			newArr = append(newArr, arr[i])
-// 		}
-// 	}
-
-// 	fmt.Println(newArr)
-// }
+			t.Errorf("contents of input differ when decoding and reencoding starting at offset %d %#x %#x", i, contents1[i], contents2[i])
+			break
+		}
+	}
+}
